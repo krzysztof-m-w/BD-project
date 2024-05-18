3575a64aa0ee221371b75f062ea37f6d558a5e16
krzysztof-m-w <krzysztof.m.weber@gmail.com>
2024-05-18 14:40:45 +0200

    add seat insertion

--- database_creator.py
+++ database_creator.py
@@ -1,4 +1,10 @@
 from cassandra.cluster import Cluster
+import string
+
+ROOMS = 2
+ROWS = 4
+SEATS_IN_ROW = 4
+
 
 cluster = Cluster(['127.0.0.2', '127.0.0.3', '127.0.0.4'], port=9042)
 session = cluster.connect('cinema')
@@ -7,9 +13,9 @@ create_seats_table_query = """CREATE TABLE IF NOT EXISTS seats(
         id int, 
         room_id int, 
         row text, 
-        number int,  
-        PRIMARY KEY(id, room_id)
-    ) WITH CLUSTERING ORDER BY (room_id ASC);"""
+        seat_number int,  
+        PRIMARY KEY(room_id, id)
+    );"""
 
 session.execute(create_seats_table_query)
 
@@ -23,6 +29,18 @@ create_reservations_table_query = """CREATE TABLE IF NOT EXISTS reservations(
 """
 
 session.execute(create_reservations_table_query)
+
+id = 0
+for room_id in range(1, ROOMS+1):
+    for row in string.ascii_uppercase[:ROWS]:
+        for seat_number in range(1, SEATS_IN_ROW):
+
+            add_query = f"""INSERT INTO seats
+            (id, room_id, row, seat_number) VALUES
+            ({id}, {room_id}, '{row}', {seat_number});"""
+            
+            session.execute(add_query)
+            id += 1
 
 session.shutdown()
 cluster.shutdown()
