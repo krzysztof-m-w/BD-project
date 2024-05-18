a953611077cabb800a45e7f0f39effa6a9be2c43
krzysztof-m-w <krzysztof.m.weber@gmail.com>
2024-05-18 13:58:29 +0200

    add keyspace creator

---
+++ keyspace_creator.py
@@ -0,0 +1,17 @@
+from cassandra.cluster import Cluster
+
+cluster = Cluster(['127.0.0.2', '127.0.0.3', '127.0.0.4'], port=9042)
+session = cluster.connect()
+
+create_keyspace_query = """
+CREATE KEYSPACE IF NOT EXISTS cinema
+WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 }
+"""
+
+session.execute(create_keyspace_query)
+
+
+session.shutdown()
+
+
+cluster.shutdown()
